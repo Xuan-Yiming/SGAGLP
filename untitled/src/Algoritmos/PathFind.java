3c9bc5a1500c84e9b3fb8a3b26cfff674bfce04e
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-09-05 11:05:33 -0500

    Update

--- untitled/src/Algoritmos/PathFind.java
+++ untitled/src/Algoritmos/PathFind.java
@@ -1,19 +1,15 @@
 package Algoritmos;
 
+import Clases.DetalleRuta;
 import Clases.Flota;
 import Clases.Mapa;
 import Clases.Pedido;
-import Clases.Ruta;
-import java.util.List;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Date;
+import java.util.List;
 
 public class PathFind {
     public void solution(Mapa mapa, List<Flota> flotas, List<Pedido> pedidos) {
-        int x = mapa.getAncho();
-        int y = mapa.getLargo();
         int n = flotas.size();
         int t = pedidos.size();
         for (int iter = 0; iter < t/n; iter++) {
@@ -41,31 +37,20 @@ public class PathFind {
             //llenar calcular costo de cada pedido para cada flota
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (canDeliver(sortedPedidos.get(1), flotas.get(i))) {
-                        cost[i][j] = getDistance(sortedPedidos.get(j), flotas.get(i));
-                    }else {
-                        cost[i][j] = 999;
-                    }
-                }
-            }
-
-            //Copiar la matriz de costos
-            double [][] source = new double[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    source[i][j] = cost[i][j];
+                    cost[i][j] = calculateCost(flotas.get(i), sortedPedidos.get(j));
                 }
             }
 
             //aplicar hungarian algorithm para obtener la asignacion de pedidos a flotas
-            HungarianAlgorithm hbm = new HungarianAlgorithm(source);
+            HungarianAlgorithm hbm = new HungarianAlgorithm(cost);
             int[] result = hbm.execute();
 
             for (int i = 0; i < n; i++) {
-                //asignar el costo de cada pedido a la flota
-                int costo = (int)cost[i][result[i]];
+                //crear detalle
+                DetalleRuta detalleRuta = new DetalleRuta(calculateDeliveryDate(flotas.get(i),sortedPedidos.get(result[i])),'E',(int)cost[i][result[i]]);
                 //agregar el pedido a la ruta
-                //flota[i] add Ruta add Pedido[result[i]];
+                detalleRuta.setNodo(sortedPedidos.get(result[i]));
+                flotas.get(i).getRutas().get(flotas.size()-1).getDetalleRutaList().add(detalleRuta);
             }
 
             //eliminar los pedidos asignados
@@ -74,11 +59,15 @@ public class PathFind {
 
 
 
-    private boolean canDeliver(Pedido pedido, Flota flota) {
+    private boolean canDeliver( Flota flota, Pedido pedido) {
         return true;
     }
 
-    private int getDistance(Pedido pedido, Flota flota) {
+    private double calculateCost( Flota flota, Pedido pedido) {
         return 0;
+    }
+
+    private Date calculateDeliveryDate( Flota flota, Pedido pedido) {
+        return null;
     }
 }
