48867350047454440b109bb82a204671c02aece1
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-09-19 22:54:54 -0500

    Update Main.java

--- ABCVRP/src/Main.java
+++ ABCVRP/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
         cars.add(new Car(10.0)); // Car 1 with a maximum capacity of 10.0
         cars.add(new Car(12.0)); // Car 2 with a maximum capacity of 12.0
         cars.add(new Car(8.0));  // Car 3 with a maximum capacity of 8.0
+
         // Add more cars with their maximum capacities as needed
 
         // Create a list of orders with their positions (positionX, positionY) and demand
@@ -42,6 +43,10 @@ public class Main {
         orders.add(new Order(14, 29, 21,14, new Date(2023, 12, 12, 1, 0, 0))); // Order 14 with demand 2
         orders.add(new Order(15, 31, 23,15, new Date(2023, 12, 12, 3, 0, 0))); // Order 15 with demand 4
         orders.add(new Order(16, 33, 25,16, new Date(2023, 12, 12, 13, 0, 0))); // Order 16 with demand 1
+        orders.add(new Order(17, 35, 27,17, new Date(2023, 12, 12, 12, 0, 0))); // Order 17 with demand 3
+        orders.add(new Order(18, 37, 29,18, new Date(2023, 12, 12, 11, 0, 0))); // Order 18 with demand 2
+        orders.add(new Order(19, 39, 31,19, new Date(2023, 12, 12, 10, 0, 0))); // Order 19 with demand 4
+        orders.add(new Order(20, 41, 33,20, new Date(2023, 12, 12, 2, 0, 0))); // Order 20 with demand 1
         // Add more orders with their positions and demand as needed
 
 //        // Create an instance of the ABCAlgorithm with the defined parameters, cars, and orders
