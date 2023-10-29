dbad9b3c4eda58ca68f912257cb7d8144708ae6f
Flaxyme11 <reidak_fabian@hotmail.com>
2023-10-29 02:46:49 -0500

    C6
    dos camiones en mov y los puestos estaticos.
    TODO: leyenda y  conectar apis

--- front-notas/src/canvas-resize/MapaGrid2.jsx
+++ front-notas/src/canvas-resize/MapaGrid2.jsx
@@ -6,6 +6,8 @@ const MapGrid2 = (props) => {
     const canvasRef = useRef(null);
     const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
     let rapidez = 1/72;
+    const [cantidadCamiones, setCantidadCamiones] = useState(0);
+    const [todosCamiones, setTodosCamiones] = useState('');
 
     let [unidadCuadroAncho, setUnidadCuadroAncho] = useState('');
     let [unidadCuadroAlto, setUnidadCuadroAlto] = useState('');
@@ -14,9 +16,10 @@ const MapGrid2 = (props) => {
     const elementosEstaticos = [{tipo: 'planta',x:12,y:8},
     {tipo: 'cisterna',x:42,y:42},
     {tipo: 'cisterna',x:63,y:3}
+    ,
+    {tipo: 'cliente',x:29,y:10},
+    {tipo: 'cliente',x:14,y:25}
     // ,
-    // {tipo: 'cliente',x:12,y:8},
-    // {tipo: 'cliente',x:12,y:8},
     // {tipo: 'cliente',x:12,y:8},
     // {tipo: 'cliente',x:12,y:8},
     // {tipo: 'cliente',x:12,y:8},
@@ -46,26 +49,26 @@ const elementosCamiones =[
 {tipo: 'camion',id:'TA01',x:28,y:10,time:19},
 {tipo: 'camion',id:'TA01',x:29,y:10,time:20},
 
-// {tipo: 'camion',id:'TA02',x:12,y:8,time:1},
-// {tipo: 'camion',id:'TA02',x:13,y:8,time:2},
-// {tipo: 'camion',id:'TA02',x:14,y:8,time:3},
-// {tipo: 'camion',id:'TA02',x:15,y:8,time:4},
-// {tipo: 'camion',id:'TA02',x:16,y:8,time:5},
-// {tipo: 'camion',id:'TA02',x:17,y:8,time:6},
-// {tipo: 'camion',id:'TA02',x:18,y:8,time:7},
-// {tipo: 'camion',id:'TA02',x:19,y:8,time:8},
-// {tipo: 'camion',id:'TA02',x:20,y:8,time:9},
-// {tipo: 'camion',id:'TA02',x:20,y:9,time:10},
-// {tipo: 'camion',id:'TA02',x:20,y:10,time:11},
-// {tipo: 'camion',id:'TA02',x:21,y:10,time:12},
-// {tipo: 'camion',id:'TA02',x:22,y:10,time:13},
-// {tipo: 'camion',id:'TA02',x:23,y:10,time:14},
-// {tipo: 'camion',id:'TA02',x:24,y:10,time:15},
-// {tipo: 'camion',id:'TA02',x:25,y:10,time:16},
-// {tipo: 'camion',id:'TA02',x:26,y:10,time:17},
-// {tipo: 'camion',id:'TA02',x:27,y:10,time:18},
-// {tipo: 'camion',id:'TA02',x:28,y:10,time:19},
-// {tipo: 'camion',id:'TA02',x:29,y:10,time:20}
+{tipo: 'camion',id:'TA02',x:12,y:8,time:1},
+{tipo: 'camion',id:'TA02',x:12,y:9,time:2},
+{tipo: 'camion',id:'TA02',x:12,y:10,time:3},
+{tipo: 'camion',id:'TA02',x:12,y:11,time:4},
+{tipo: 'camion',id:'TA02',x:12,y:12,time:5},
+{tipo: 'camion',id:'TA02',x:12,y:13,time:6},
+{tipo: 'camion',id:'TA02',x:12,y:14,time:7},
+{tipo: 'camion',id:'TA02',x:12,y:15,time:8},
+{tipo: 'camion',id:'TA02',x:12,y:16,time:9},
+{tipo: 'camion',id:'TA02',x:13,y:16,time:10},
+{tipo: 'camion',id:'TA02',x:14,y:16,time:11},
+{tipo: 'camion',id:'TA02',x:14,y:17,time:12},
+{tipo: 'camion',id:'TA02',x:14,y:18,time:13},
+{tipo: 'camion',id:'TA02',x:14,y:19,time:14},
+{tipo: 'camion',id:'TA02',x:14,y:20,time:15},
+{tipo: 'camion',id:'TA02',x:14,y:21,time:16},
+{tipo: 'camion',id:'TA02',x:14,y:22,time:17},
+{tipo: 'camion',id:'TA02',x:14,y:23,time:18},
+{tipo: 'camion',id:'TA02',x:14,y:24,time:19},
+{tipo: 'camion',id:'TA02',x:14,y:25,time:20}
 
 ]
 
@@ -105,13 +108,78 @@ const elementosCamiones =[
 
     }
 
-
-    const dibujarHacia2 = (context,inicio,final,velocidad)=>{
+    
+    const graficarPuntosFijos = (c)=>{
 
         
 
+
+        for (let i = 0; i < elementosEstaticos.length; i++){
+            const elemento = elementosEstaticos[i];
+
+            if(elemento.tipo == 'planta'){
+                //PLANTA
+                c.beginPath();
+                c.arc(elemento.x*unidadCuadroAncho,elemento.y*unidadCuadroAlto,unidadCuadroAncho/2,0,2*Math.PI,true);
+                c.strokeStyle='bla<ck';
+                c.fillStyle = 'green'; // Fill color
+                c.fill();
+                c.stroke();
+            }else if(elemento.tipo == 'cisterna'){
+                //Cisterna
+                c.beginPath();
+                c.arc(elemento.x*unidadCuadroAncho,elemento.y*unidadCuadroAlto,unidadCuadroAncho/2,0,2*Math.PI,true);
+                c.strokeStyle='black';
+                c.fillStyle = 'orange'; // Fill color
+                c.fill();
+                c.stroke();
+            }else if(elemento.tipo == 'cliente'){
+                c.beginPath();
+                c.moveTo((elemento.x)*unidadCuadroAncho,(elemento.y-0.5)*unidadCuadroAlto); // Move to the starting point
+                
+                // Draw the diamond shape using lines
+                c.lineTo((elemento.x)*unidadCuadroAncho - unidadCuadroAncho / 2, (elemento.y-0.5)*unidadCuadroAlto + unidadCuadroAncho / 2); // Left point
+                c.lineTo((elemento.x)*unidadCuadroAncho , (elemento.y-0.5)*unidadCuadroAlto + unidadCuadroAncho); // Bottom point
+                c.lineTo((elemento.x)*unidadCuadroAncho  + unidadCuadroAncho / 2, (elemento.y-0.5)*unidadCuadroAlto + unidadCuadroAncho / 2); // Right point
+                
+                // Close the path to complete the shape
+                c.closePath();
+                
+                // Set the fill and stroke styles
+                c.fillStyle = 'blue'; // Fill color
+                c.strokeStyle = 'black'; // Stroke color
+                
+                // Fill and stroke the diamond
+                c.fill();
+                c.stroke();
+            }
+
+        }
+
+
+
     }
 
+
+
+    const getCantidadCamiones =()=>{
+
+        let cantidadCamiones=0;
+        let camionAnt = 'aaa';
+        let camiones = [];
+
+        for (let i = 0; i < elementosCamiones.length; i++){
+            const camionEscogido = elementosCamiones[i];
+            if(camionAnt!=camionEscogido.id){
+                camionAnt = camionEscogido.id;
+                cantidadCamiones++;
+                camiones.push(camionAnt);
+                console.log(camionEscogido.id);
+            }
+        }
+        setTodosCamiones(camiones);
+        setCantidadCamiones(cantidadCamiones);
+    }
 
     // useEffect(() => {
     //     // Maneja los cambios en los datos aquí
@@ -125,6 +193,8 @@ const elementosCamiones =[
 
 
     useEffect(() => {
+        
+        getCantidadCamiones();
         // Ajustar el tamaño del canvas al del contenedor
         function updateSize() {
             if (canvasRef.current && canvasRef.current.parentElement) {
@@ -134,11 +204,13 @@ const elementosCamiones =[
         }
 
         updateSize();
+        
 
         // Agregar listener para detectar cambios de tamaño y actualizar el canvas
         window.addEventListener('resize', updateSize);
         return () => window.removeEventListener('resize', updateSize);
 
+        
     }, []);
 
     useEffect(() => {
@@ -157,7 +229,11 @@ const elementosCamiones =[
 
         var x=0;
 
+
+
+
         function animate(){
+            
         
             //seccion de medidas
             // un cuadro es una separacion canvas.width / cuadrosAncho
@@ -167,12 +243,6 @@ const elementosCamiones =[
 
             unidadCuadroAncho = canvas.width / cuadrosAncho;
             unidadCuadroAlto = canvas.height / cuadrosAlto;
-
-
-            //EL RETO AHORA ES PONERLO EN UNA CELDA QUE ADJACENTE QUE NOS MANDEN, DE 0,0 A 0,1 A 0,2 ETC
-
-            const consecutivePoints = [{"x":0,"y":1},{"x":0,"y":2},{"x":0,"y":3}]
-
         
             requestAnimationFrame(animate);
             ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -191,62 +261,40 @@ const elementosCamiones =[
             }
             ctx.strokeStyle = "black";
             ctx.stroke();
-            //termina cuadricula
-        
-            // console.log(x);
+
+            ctx.beginPath();
+            graficarPuntosFijos(ctx);
+            ctx.stroke();
 
 
-            // ctx.beginPath();
+            for(let j=0;j<cantidadCamiones;j++){
+
+                for (let i = 0; i < elementosCamiones.length; i++) {
+
+                    const camion = elementosCamiones[i];
+                    // let idAnt = 'aaa';
+                    let clockActual=0;
+                    if(camion.id==todosCamiones[j] && Math.floor(props.tiempo/72)==camion.time){
+                    // if(camion.id==todosCamiones[j] && clockActual*72<props.tiempo && props.tiempo < camion.time*72){
+                    // if(camion.id != idAnt && clockActual*72<props.tiempo && props.tiempo < camion.time*72){
+                        ctx.fillStyle = 'grey';
+                        // console.log(idAnt);
+                        // console.log(camion.id);
+                        // console.log(props.tiempo);
     
-            // ctx.arc(300,300,30,0,Math.PI,true);
-            // ctx.strokeStyle='red';
-            // ctx.stroke();
+                        // console.log(camion.x);
+                        // console.log(camion.y);
+                        ctx.fillRect( unidadCuadroAncho*(-0.5 +  camion.x),unidadCuadroAlto*(-0.5+camion.y),
+                                                unidadCuadroAncho,unidadCuadroAlto);
+                        // idAnt=camion.id;
+                        clockActual=Math.floor(props.tiempo/72);
+                        break;
+                    }
     
-            // ctx.beginPath();
-            
-            
-
-            //el anochde del canvas que divide a delta es hasta donde llegara, ese sera nuestro punto de finalizacion
-
-            const delta = (x)%canvas.width;
-
-            // ctx.arc(delta,300,30,0,Math.PI,true);
-            // ctx.strokeStyle='blue';
-            // ctx.stroke();
-
-            // ctx.fillRect(props.tiempo + unidadCuadroAncho*0.5,0 +unidadCuadroAlto*0.5,
-            //                     unidadCuadroAncho,unidadCuadroAlto);
-
-
-            // console.log(props.tiempo);
-
-            //dibujarHacia(ctx,actual,inicio,final,props.tiempo,1);
-
-            for (let i = 0; i < elementosCamiones.length; i++) {
-
-                const camion = elementosCamiones[i];
-                let idAnt = 'aaa';
-                let clockActual=0;
-                if(camion.id != idAnt && clockActual*72<props.tiempo && props.tiempo < camion.time*72){
-                    ctx.fillStyle = 'grey';
-                    // console.log(idAnt);
-                    // console.log(camion.id);
-                    // console.log(props.tiempo);
-
-                    // console.log(camion.x);
-                    // console.log(camion.y);
-                    ctx.fillRect( unidadCuadroAncho*(0.5 +  camion.x),unidadCuadroAlto*(0.5+camion.y),
-                                            unidadCuadroAncho,unidadCuadroAlto);
-                    idAnt=camion.id;
-                    clockActual=Math.floor(props.tiempo/72);
-                    break;
                 }
-                // console.log(`Truck ID: ${camion.id}`);
-                // console.log(`Type: ${camion.tipo}`);
-                // console.log(`Coordinates (x, y): (${camion.x}, ${camion.y})`);
-                // console.log(`Time: ${camion.time}`);
-                // console.log('------------------');
             }
+
+
 
         }
         
