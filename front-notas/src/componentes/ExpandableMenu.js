e5857da75e41babde484a86945034b0af12465f9
Flaxyme11 <reidak_fabian@hotmail.com>
2023-11-01 22:01:54 -0500

    c1

--- front-notas/src/componentes/ExpandableMenu.js
+++ front-notas/src/componentes/ExpandableMenu.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import '../hojas-estilo/ExpandableMenu.css';
 import logosga from "../imagenes/logoSGA.png";
 
-const ExpandableMenu = () => {
+const ExpandableMenu = (props) => {
   const [menuExpanded, setMenuExpanded] = useState(false);
   const menuRef = useRef(null);
 
@@ -27,6 +27,43 @@ const ExpandableMenu = () => {
 
 //   <i class="bi bi-calendar3"></i>
 
+
+
+  const handleComponente1 = () =>{
+
+    props.cambiarComponente(true);
+    props.cambiarComponente2(false);
+    props.cambiarComponente3(false);
+    props.cambiarComponente4(false);
+
+
+  }
+
+  const handleComponente2 = () =>{
+
+    props.cambiarComponente(false);
+    props.cambiarComponente2(true);
+    props.cambiarComponente3(false);
+    props.cambiarComponente4(false);
+    
+  }
+  const handleComponente3 = () =>{
+
+    props.cambiarComponente(false);
+    props.cambiarComponente2(false);
+    props.cambiarComponente3(true);
+    props.cambiarComponente4(false);
+    
+  }
+  const handleComponente4 = () =>{
+
+    props.cambiarComponente(false);
+    props.cambiarComponente2(false);
+    props.cambiarComponente3(false);
+    props.cambiarComponente4(true);
+    
+  }
+
   return (
     <div className={`expandable-menu ${menuExpanded ? 'expanded' : ''}`} ref={menuRef}>
       <div className="toggle-button" onClick={toggleMenu}>
@@ -37,10 +74,10 @@ const ExpandableMenu = () => {
       <div className="menu-content">
         <ul>
         
-          <li>{menuExpanded ?     <div><i class="bi bi-calendar3"></i> Planificacion</div>:<i class="bi bi-calendar3"></i>  }</li>
-          <li>{menuExpanded ?     <div><i class="bi bi-box-seam"></i> Pedidos</div>:<i class="bi bi-box-seam"></i>  }</li>
-          <li>{menuExpanded ?     <div><i class="bi bi-truck"></i> Flotas</div>:<i class="bi bi-truck"></i> }</li>
-          <li>{menuExpanded ?     <div><i class="bi bi-stopwatch"></i> Simulaciones</div>:<i class="bi bi-stopwatch"></i> }</li>
+          <li>{menuExpanded ?     <div onClick={handleComponente1}><i class="bi bi-calendar3"></i> Planificacion</div>:<i class="bi bi-calendar3"></i>  }</li>
+          <li>{menuExpanded ?     <div onClick={handleComponente2}><i class="bi bi-box-seam"></i> Pedidos</div>:<i class="bi bi-box-seam"></i>  }</li>
+          <li>{menuExpanded ?     <div onClick={handleComponente3}><i class="bi bi-truck"></i> Flotas</div>:<i class="bi bi-truck"></i> }</li>
+          <li>{menuExpanded ?     <div onClick={handleComponente4}><i class="bi bi-stopwatch"></i> Simulaciones</div>:<i class="bi bi-stopwatch"></i> }</li>
         </ul>
       </div>
     </div>
