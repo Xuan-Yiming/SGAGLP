4e21b2ed83fc07630005fd28b3360f7960b85841
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-11-07 18:58:35 -0500

    Ordenar

--- DP1_5Ev2/DP15E/src/main/java/pe/com/pucp/DP15E/GeneticAlgorithms/Problem/SolucionRuta.java
+++
@@ -1,9 +0,0 @@
-package pe.com.pucp.DP15E.GeneticAlgorithms.Problem;
-
-public class SolucionRuta {
-    public char tipo;
-    public String id;
-    public int x;
-    public int y;
-    public int time;
-}
