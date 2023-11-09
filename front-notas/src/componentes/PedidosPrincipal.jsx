22d6c4285b94973bb9454b948ae743a9735d7a41
Flaxyme11 <reidak_fabian@hotmail.com>
2023-11-09 00:44:05 -0500

    c3

--- front-notas/src/componentes/PedidosPrincipal.jsx
+++ front-notas/src/componentes/PedidosPrincipal.jsx
@@ -1,10 +1,44 @@
 import React, { useEffect, useState,useRef  } from 'react';
 import '../hojas-estilo/PedidosPrincipal.css';
 import axios from 'axios';
+import { Button, Modal } from "react-bootstrap";
 
 export function PedidosPrincipal() {
     
+    const [query, setQuery] = useState('');
+    const [mostrarModal, setmostrarModal] = useState(false);
+    const [editable, setEditable] = useState(false);
 
+    const handleSearch = () => {
+        // const filteredResults = data.filter(item =>
+        //   item.name.toLowerCase().includes(query.toLowerCase())
+        // );
+        // setResults(filteredResults);
+      }
+
+      
+    const handleModalClose = () => {
+        setmostrarModal(false);
+        setEditable(false);
+        /*
+        setDescripcion(datosCompetencia.descripcionCompetencia);
+        setEvidencia(datosCompetencia.evidenciaCompetencia);
+        */
+    };
+    const handleModalAceptar = async () => {
+
+
+
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    };
+
+
+    const crearNuevoPedido = async (event) => {
+        setmostrarModal(true);
+    };
 
     return (
         <div className='principalPedidos'>
@@ -15,11 +49,19 @@ export function PedidosPrincipal() {
                 <div className='tituloPedidosDer'>
                     <div className='seccionBotonesPedidos'>
                         <div className='botonIzqPedidos'>
-
+                            <button >Carga Masiva</button>
                         </div>
                         <div className='botonDerPedidos'>
-
+                            <button onClick={crearNuevoPedido}>Nuevo Pedido</button>
+                            <Modal show={mostrarModal} onClose={handleModalClose}>
+                                <div className='botonModalCerrar'>
+                                    <button onClick={handleModalClose}>X</button>
+                                </div>
+                                <h2>Modal Content</h2>
+                                <p>This is a simple modal example.</p>
+                            </Modal>
                         </div>
+
                     </div>
                 </div>
             </div>
@@ -28,8 +70,16 @@ export function PedidosPrincipal() {
                 <div className='contendorBuscador'>
                     <div className='mitadContenedorBuscador'>
                         <div className='mitadBuscadorIzq'>
-                            Que estas bucando?
-                            
+                            <div>Que estas bucando?</div>
+                            <div className='buscadorQuery'>
+                                <input
+                                    type="text"
+                                    placeholder="Buscar productos"
+                                    value={query}
+                                    onChange={(e) => setQuery(e.target.value)}
+                                />
+                                <button onClick={handleSearch}>Buscar</button>
+                            </div>
                         </div>
                         <div className='mitadBuscadorDer'>
                             
