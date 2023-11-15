0872e34517f17dd1c021bc346a10472b22cde1ea
joelAquino18 <aquino.joel@pucp.edu.pe>
2023-11-15 12:49:28 -0500

    Back

---
+++ DP1_5Ev7/DP15E/src/main/java/pe/com/pucp/DP15E/GeneticAlgorithms/Gene.java
@@ -0,0 +1,359 @@
+package pe.com.pucp.DP15E.GeneticAlgorithms;
+
+import pe.com.pucp.DP15E.GeneticAlgorithms.Problem.Node;
+import pe.com.pucp.DP15E.GeneticAlgorithms.Problem.Vehicle;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Gene implements Cloneable {
+    private static final int MINUTES_IN_A_DAY = 1440;
+    private int id;
+    private Point posicion;
+    private char type;
+    private double pesoBruto; // ton
+    private double cargaGLP; // peso carga GLP = carga GLP/2 ton
+    private double pesoNeto; // ton
+    private double velocidad = 50; // 50 km/h
+    private double cargaPetroleo = 25; // 25 Galones
+    private Date mantenimiento; // 1 mes
+    private ArrayList<Node> route;
+    private int totalTime;
+
+    // Constructor
+
+    public Gene() {
+    }
+
+    public Gene(Vehicle vehicle, Node depot) {
+        this.id = vehicle.getId();
+        this.posicion = vehicle.getPosicion();
+        this.type = vehicle.getType();
+        this.pesoBruto = vehicle.getPesoBruto();
+        this.cargaGLP = vehicle.getCargaGLP();
+        this.pesoNeto = vehicle.getPesoNeto();
+        this.velocidad = vehicle.getVelocidad();
+        this.cargaPetroleo = vehicle.getCargaPetroleo();
+        this.mantenimiento = vehicle.getMantenimiento();
+        this.route = new ArrayList<>();
+        this.route.add(depot);
+        this.totalTime = vehicle.getTotalTime();
+    }
+
+    // Methods
+
+    public void print() {
+        System.out.print("Vehicle: " + this.id);
+        System.out.print(" - Total time: " + totalTime);
+        System.out.println(" - Fitness: " + calculateFitness());
+        System.out.println("Route: ");
+        for (int i = 0; i < route.size(); i++) {
+            // print in the same line saparated by ' - '
+            if (i != 0) {
+                System.out.print(route.get(i).getId() + ": (" + route.get(i).getPosicion().getX() + ", "
+                        + route.get(i).getPosicion().getY() + ") - ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("===================================================");
+    }
+
+    public void insertDepot(ArrayList<Node> depots) {
+        ArrayList<Node> newRoute = new ArrayList<>();
+
+        Point _posicion = this.posicion;
+        double _cargaGLP = this.cargaGLP;
+        double _pesoNeto = this.pesoNeto;
+        double _cargaPetroleo = this.cargaPetroleo;
+        int _totalTime = this.totalTime;
+
+        for (int i = 0; i < route.size(); i++) {
+            if (canDeliver(route.get(i))) {
+                newRoute.add(route.get(i));
+                this.cargaGLP -= route.get(i).getCantidad();
+                this.cargaPetroleo -= consumoGLP(distanceToANode(route.get(i)));
+                this.pesoNeto = this.pesoBruto + this.cargaGLP / 2;
+                this.totalTime += timeToANode(distanceToANode(route.get(i)));
+                this.posicion = route.get(i).getPosicion();
+            } else {
+                Node bestDepot = bestDepot(depots);
+                newRoute.add(bestDepot);
+
+                double maxCapacidadGLP = 0;
+                switch (this.type) {
+                    case 'A':
+                        maxCapacidadGLP = 25;
+                        break;
+                    case 'B':
+                        maxCapacidadGLP = 15;
+                        break;
+                    case 'C':
+                        maxCapacidadGLP = 10;
+                        break;
+                    case 'D':
+                        maxCapacidadGLP = 5;
+                        break;
+                }
+                bestDepot.setCapacidad(bestDepot.getCapacidad() - (maxCapacidadGLP - this.cargaGLP));
+
+                switch (this.type) {
+                    case 'A':
+                        this.pesoBruto = 2.5;
+                        this.cargaGLP = 25;
+                        break;
+                    case 'B':
+                        this.pesoBruto = 2;
+                        this.cargaGLP = 15;
+                        break;
+                    case 'C':
+                        this.pesoBruto = 1.5;
+                        this.cargaGLP = 10;
+                        break;
+                    case 'D':
+                        this.pesoBruto = 1;
+                        this.cargaGLP = 5;
+                        break;
+                }
+                this.cargaPetroleo = 25;
+                newRoute.add(route.get(i));
+            }
+        }
+        
+        this.posicion = _posicion;
+        this.totalTime = _totalTime;
+        this.cargaGLP = _cargaGLP;
+        this.pesoNeto = _pesoNeto;
+        this.cargaPetroleo = _cargaPetroleo;
+
+        this.route = newRoute;
+    }
+    
+    public void removeDepots() {
+        for (int i = 0; i < route.size(); i++) {
+            if (route.get(i).getTipo() == 'D') {
+                route.remove(i);
+                i--;
+            }
+        }
+    }
+
+
+    public double calculateFitness() {
+        double fitness = 0;
+        Point _posicion = this.posicion;
+        double _cargaGLP = this.cargaGLP;
+        double _pesoNeto = this.pesoNeto;
+        double _cargaPetroleo = this.cargaPetroleo;
+        int _totalTime = this.totalTime;
+
+        for (int i = 0; i < route.size() - 1; i++) {
+            // if can deliver to this node then fitness +1
+            if (route.get(i) == null) {
+                continue;
+            }
+            if (canDeliver(route.get(i))) {
+                this.cargaGLP -= route.get(i).getCantidad();
+                this.cargaPetroleo -= consumoGLP(distanceToANode(route.get(i)));
+                this.pesoNeto = this.pesoBruto + this.cargaGLP / 2;
+                this.totalTime += timeToANode(distanceToANode(route.get(i)));
+                this.posicion = route.get(i).getPosicion();
+                if (route.get(i).getTipo() == 'C') {
+                    fitness++;
+                } else if (route.get(i).getTipo() == 'D') {
+                    switch (this.type) {
+                        case 'A':
+                            this.pesoBruto = 2.5;
+                            this.cargaGLP = 25;
+                            break;
+                        case 'B':
+                            this.pesoBruto = 2;
+                            this.cargaGLP = 15;
+                            break;
+                        case 'C':
+                            this.pesoBruto = 1.5;
+                            this.cargaGLP = 10;
+                            break;
+                        case 'D':
+                            this.pesoBruto = 1;
+                            this.cargaGLP = 5;
+                            break;
+                    }
+                    this.cargaPetroleo = 25;
+                }
+            } else {
+                break;
+            }
+            // fitness += Math.sqrt(Math.pow(route.get(i).getPosicion().getX() -
+            // route.get(i+1).getPosicion().getX(), 2) +
+            // Math.pow(route.get(i).getPosicion().getY() -
+            // route.get(i+1).getPosicion().getY(), 2));
+        }
+        this.posicion = _posicion;
+        this.totalTime = _totalTime;
+        this.cargaGLP = _cargaGLP;
+        this.pesoNeto = _pesoNeto;
+        this.cargaPetroleo = _cargaPetroleo;
+        return fitness;
+    }
+
+    public double consumoGLP(double distancia) {
+        return distancia * pesoNeto / 180;
+    }
+
+    public void addNode(Node node) {
+        this.route.add(node);
+    }
+
+    public boolean canDeliver(Node custumor) {
+        int distancia = distanceToANode(custumor);
+        if (distancia != 0) {
+            if (custumor.getTipo() == 'C') {
+                // si tiene suficiente GLP
+                if (this.cargaGLP < custumor.getCantidad()) {
+                    return false;
+                }
+                // si puede llegar antes de que termine el dia
+                if (totalTime + timeToANode(distancia) > MINUTES_IN_A_DAY) {
+                    return false;
+                }
+                // si puede llegar a tiempo
+                if (totalTime + timeToANode(
+                        distancia) > (custumor.getFechaFinal().getTime() - custumor.getFechaInicio().getTime())
+                                / 60000) {
+                    return false;
+                }
+            }
+
+            // si no tiene suficiente petroleo
+            if (this.cargaPetroleo < consumoGLP(distancia)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int distanceToANode(Node node) {
+        return (int) (Math.abs(node.getPosicion().getX() - this.getPosicion().getX())  + Math.abs(node.getPosicion().getY() - this.getPosicion().getY()));
+    }
+
+    public int timeToANode(int distancia) {
+        // time to a node in minutes
+        return (int) (distancia / this.velocidad * 60);
+    }
+
+    public Node bestDepot(ArrayList<Node> depots) {
+        int shortestDistance = Integer.MAX_VALUE;
+        Node bestDepot = null;
+        for (Node depot : depots) {
+            if (canDeliver(depot)) {
+                if (distanceToANode(depot) < shortestDistance) {
+                    shortestDistance = distanceToANode(depot);
+                    bestDepot = depot;
+                }
+            }
+        }
+        return bestDepot;
+    }
+
+
+    @Override
+    public Gene clone() {
+        try {
+            return (Gene) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // This should not happen.
+        }
+    }
+
+    // getters and setters
+
+    public Point getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Point posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public double getPesoBruto() {
+        return pesoBruto;
+    }
+
+    public void setPesoBruto(double pesoBruto) {
+        this.pesoBruto = pesoBruto;
+    }
+
+    public double getCargaGLP() {
+        return cargaGLP;
+    }
+
+    public void setCargaGLP(double cargaGLP) {
+        this.cargaGLP = cargaGLP;
+    }
+
+    public double getPesoNeto() {
+        return pesoNeto;
+    }
+
+    public void setPesoNeto(double pesoNeto) {
+        this.pesoNeto = pesoNeto;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public double getCargaPetroleo() {
+        return cargaPetroleo;
+    }
+
+    public void setCargaPetroleo(double cargaPetroleo) {
+        this.cargaPetroleo = cargaPetroleo;
+    }
+
+    public Date getMantenimiento() {
+        return mantenimiento;
+    }
+
+    public void setMantenimiento(Date mantenimiento) {
+        this.mantenimiento = mantenimiento;
+    }
+
+    public ArrayList<Node> getRoute() {
+        return route;
+    }
+
+    public void setRoute(ArrayList<Node> route) {
+        this.route = route;
+    }
+}
---
+++ DP1_5Ev7/DP15E/src/main/java/pe/com/pucp/DP15E/model/Node.java
@@ -0,0 +1,243 @@
+package pe.com.pucp.DP15E.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+//@NoArgsConstructor
+@Data
+@Entity(name="Node")
+@Table(name="nodo_simulacion")
+@SecondaryTable(name = "pedido_simulacion", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fid_nodo"))
+@SecondaryTable(name = "depot_simulacion", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fid_nodo"))
+@SecondaryTable(name = "bloqueo_simulacion", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fid_nodo"))
+public class Node implements Serializable , Cloneable {
+    private static final Integer serialVersionUID= 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_nodo")
+    private Integer id;
+    @Column(name="coordenadaX")
+    private int x;
+    @Column(name="coordenadaY")
+    private int y;
+    //private Point posicion;
+    @Column(name="tipo")
+    private char tipo; // C: Customer, D: Depot, B: Block
+
+
+
+
+    @Column(name = "activo")
+    private boolean activo;
+    // Block
+
+    @Column(name = "fecha_inicio", table = "bloqueo_simulacion")
+    private String fechaInicio;
+
+    @Column(name = "fecha_final", table = "bloqueo_simulacion")
+    private String fechaFinal;
+
+    // Customer
+
+    @Column(name = "cantidad", table = "pedido_simulacion")
+    private Double cantidad;
+
+    @Column(name = "fid_cliente", table = "pedido_simulacion")
+    private String idCliente;
+
+    @Column(name = "fecha_origen", table = "pedido_simulacion")
+    private LocalDateTime fechaOrigen;
+
+    @Column(name = "hora_demandada", table = "pedido_simulacion")
+    private Integer horaDemandada;
+    // Depot
+
+    @Column(name = "capacidad", table = "depot_simulacion")
+    private Double capacidad;
+
+
+    // Constructor
+    public Node(int x, int y,String fechaInicio,String fechaFinal) {
+        // Block
+        //this.posicion = new Point(x, y);
+        this.x=x;
+        this.y=y;
+        this.tipo = 'B';
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+    }
+
+
+    public Node(int x, int y, double cantidad,
+                LocalDateTime fechaOrigen, String idCliente, int horaDemandada) {
+        // Customer
+
+        //this.id = id;
+        //this.posicion = new Point(x, y);
+        this.x=x;
+        this.y=y;
+        this.tipo = 'C';
+        this.cantidad = cantidad;
+        //this.fechaInicio = fechaInicio;
+        //this.fechaFinal = fechaFinal;
+        this.fechaOrigen = fechaOrigen;
+        this.idCliente = idCliente;
+        this.horaDemandada = horaDemandada;
+    }
+
+    public Node(){
+        this.id = 0;
+        //this.posicion = new Point(x, y);
+        this.x=0;
+        this.y=0;
+        this.tipo = 'A';
+        this.cantidad = 0.0;
+        this.activo = false;
+        this.capacidad=0.0;
+        //this.fechaInicio = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
+        //this.fechaFinal = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
+        this.fechaInicio = "2023-01-01 00:00:00";
+        this.fechaFinal = "2023-01-01 00:00:00";
+        this.fechaOrigen = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
+        this.idCliente = " ";
+        this.horaDemandada = 0;
+    }
+    public Node(int x, int y, double capacidad) {
+
+        // Depot
+        //this.posicion = new Point(x, y);
+        this.x=x;
+        this.y=y;
+        this.tipo = 'D';
+        this.capacidad = capacidad;
+        //this.fechaInicio = new Date();
+        //this.fechaFinal = new Date();
+    }
+
+    @Override
+    public Node clone() {
+        try {
+            return (Node) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();  // This should not happen.
+        }
+    }
+
+    //getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    //public Point getPosicion() {
+    //    return posicion;
+    //}
+
+    //public void setPosicion(Point posicion) {
+    //   this.posicion = posicion;
+    //}
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(String fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFechaOrigen() {
+        return fechaOrigen;
+    }
+
+    public void setFechaOrigen(LocalDateTime fechaOrigen) {
+        this.fechaOrigen = fechaOrigen;
+    }
+
+    public Integer getHoraDemandada() {
+        return horaDemandada;
+    }
+
+    public void setHoraDemandada(Integer horaDemandada) {
+        this.horaDemandada = horaDemandada;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
