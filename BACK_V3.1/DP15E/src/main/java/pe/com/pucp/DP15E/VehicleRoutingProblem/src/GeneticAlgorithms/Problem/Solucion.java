58fc4cda7cc293ae980f1afa2076d9f1d695952a
joelAquino18 <aquino.joel@pucp.edu.pe>
2023-12-08 23:04:08 -0500

    Update Solucion.java

--- BACK_V3.1/DP15E/src/main/java/pe/com/pucp/DP15E/VehicleRoutingProblem/src/GeneticAlgorithms/Problem/Solucion.java
+++ BACK_V3.1/DP15E/src/main/java/pe/com/pucp/DP15E/VehicleRoutingProblem/src/GeneticAlgorithms/Problem/Solucion.java
@@ -1,6 +1,6 @@
 package pe.com.pucp.DP15E.VehicleRoutingProblem.src.GeneticAlgorithms.Problem;
 
-import GeneticAlgorithms.*;
+import pe.com.pucp.DP15E.VehicleRoutingProblem.src.GeneticAlgorithms.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
