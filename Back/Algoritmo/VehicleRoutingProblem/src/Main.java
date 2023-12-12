1d7e94aac0352764e02aa216eae76863920256fd
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-12-12 12:43:24 -0500

    update algoritm

--- Back/Algoritmo/VehicleRoutingProblem/src/Main.java
+++ Back/Algoritmo/VehicleRoutingProblem/src/Main.java
@@ -25,17 +25,17 @@ public class Main {
             //create a start date
             Calendar calendar_s = Calendar.getInstance();
             calendar_s.set(Calendar.YEAR, 2023);
-            calendar_s.set(Calendar.MONTH, 6);
-            calendar_s.set(Calendar.DAY_OF_MONTH, 10);
-            calendar_s.set(Calendar.HOUR_OF_DAY, 1);
-            calendar_s.set(Calendar.MINUTE, 1);
+            calendar_s.set(Calendar.MONTH, 11);
+            calendar_s.set(Calendar.DAY_OF_MONTH, 12);
+            calendar_s.set(Calendar.HOUR_OF_DAY, 12);
+            calendar_s.set(Calendar.MINUTE, 21);
 
             Calendar calendar_e = Calendar.getInstance();
             calendar_e.set(Calendar.YEAR, 2023);
-            calendar_e.set(Calendar.MONTH, 6);
-            calendar_e.set(Calendar.DAY_OF_MONTH, 10);
-            calendar_e.set(Calendar.HOUR_OF_DAY, 22);
-            calendar_e.set(Calendar.MINUTE, 49);
+            calendar_e.set(Calendar.MONTH, 11);
+            calendar_e.set(Calendar.DAY_OF_MONTH, 12);
+            calendar_e.set(Calendar.HOUR_OF_DAY, 12);
+            calendar_e.set(Calendar.MINUTE, 27);
 
 
             ArrayList<CurrentVehicle> currentVehicles = new ArrayList<CurrentVehicle>();
@@ -68,9 +68,9 @@ public class Main {
 
             String data = gson.toJson(inputData);
 
-            data = "{\"vehiculos\":[],\"pedidos\":[]}";
+            data = "{\"vehiculos\":[{\"id\":\"TA1\",\"x\":12,\"y\":8,\"pedido\":\"C999\"},{\"id\":\"TA2\",\"x\":12,\"y\":8,\"pedido\":\"C998\"},{\"id\":\"TB3\",\"x\":12,\"y\":8,\"pedido\":\"C997\"},{\"id\":\"TB4\",\"x\":12,\"y\":8,\"pedido\":\"C996\"},{\"id\":\"TB5\",\"x\":12,\"y\":8,\"pedido\":\"C995\"},{\"id\":\"TB6\",\"x\":12,\"y\":8,\"pedido\":\"C994\"},{\"id\":\"TC7\",\"x\":12,\"y\":8,\"pedido\":\"C993\"},{\"id\":\"TC8\",\"x\":12,\"y\":8,\"pedido\":\"C992\"},{\"id\":\"TC9\",\"x\":12,\"y\":8,\"pedido\":\"C991\"},{\"id\":\"TC10\",\"x\":12,\"y\":8,\"pedido\":\"C990\"},{\"id\":\"TD11\",\"x\":12,\"y\":8,\"pedido\":\"C989\"},{\"id\":\"TD12\",\"x\":12,\"y\":8,\"pedido\":\"C988\"},{\"id\":\"TD13\",\"x\":12,\"y\":8,\"pedido\":\"C987\"},{\"id\":\"TD14\",\"x\":12,\"y\":8,\"pedido\":\"C986\"},{\"id\":\"TD15\",\"x\":12,\"y\":8,\"pedido\":\"C985\"},{\"id\":\"TD16\",\"x\":12,\"y\":8,\"pedido\":\"C984\"},{\"id\":\"TD17\",\"x\":12,\"y\":8,\"pedido\":\"C983\"},{\"id\":\"TD18\",\"x\":12,\"y\":8,\"pedido\":\"C982\"},{\"id\":\"TD19\",\"x\":12,\"y\":8,\"pedido\":\"C981\"},{\"id\":\"TD20\",\"x\":12,\"y\":8,\"pedido\":\"C980\"}],\"pedidos\":[{\"id\":\"C999\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":25},{\"id\":\"C998\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":25},{\"id\":\"C997\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":15},{\"id\":\"C996\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":15},{\"id\":\"C995\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":15},{\"id\":\"C994\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":15},{\"id\":\"C993\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":10},{\"id\":\"C992\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":10},{\"id\":\"C991\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":10},{\"id\":\"C990\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":10},{\"id\":\"C989\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C988\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C987\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C986\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C985\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C984\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C983\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C982\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C981\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5},{\"id\":\"C980\",\"x\":69,\"y\":49,\"start\":\"2023-12-01T16:25:00.000Z\",\"end\":\"2023-12-02T02:25:00.000Z\",\"glp\":5}]}";
             
-            algorithmVRP = new GeneticAlgorithmVRP(calendar_s.getTime(), calendar_e.getTime(),data,'S',40);
+            algorithmVRP = new GeneticAlgorithmVRP(calendar_s.getTime(), calendar_e.getTime(),data,'P',40);
 
             Solucion solucion = algorithmVRP.getSolucion();
 
